fb6a7cc55a7f9620c16856d55a9ed888d51ecaaa
Dudi <dudizimber@hotmail.com>
2020-04-01 10:50:19 +0300

    //obj = obj || new exports();     obj = obj || {};

--- src/model/Money.js
+++ src/model/Money.js
@@ -43,7 +43,8 @@ var exports = function() {
  */
 exports.constructFromObject = function(data, obj) {
   if (data) {
-    obj = obj || new exports();
+    //obj = obj || new exports();
+    obj = obj || {};
 
       if (data.hasOwnProperty('amount')) {
       obj['amount'] = ApiClient.convertToType(data['amount'], 'Number');
