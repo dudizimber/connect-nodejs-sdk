097fabf8e59ce2de1c9e5a68e427f97d7fbf392f
Dudi <dudizimber@hotmail.com>
2020-04-01 12:34:56 +0300

    fixed comments

--- src/model/Card.js
+++ src/model/Card.js
@@ -53,7 +53,7 @@ var exports = function() {
  */
 exports.constructFromObject = function(data, obj) {
   if (data) {
-    ////obj = obj || new exports();
+    //obj = obj || new exports();
     obj = obj || {};
 
       if (data.hasOwnProperty('id')) {
